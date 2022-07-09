d4d74063c37147c06906b026efaacde89dce6d52
vera.k <vera_boris@tut.by>
2022-07-09 19:38:05 +0300

    add fixtures

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,10 +1,16 @@
-import argparse
+from gendiff.cli import parse
+from gendiff.main_logic.diff import generate_diff
 
 
-def parse():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
-    args = parser.parse_args()
-    return args
+def main():
+    parser = parse()
+    difference = generate_diff(
+        parser.first_file,
+        parser.second_file,
+        parser.format
+    )
+    print (difference)
+
+
+if __name__ == '__main__':
+    main()
