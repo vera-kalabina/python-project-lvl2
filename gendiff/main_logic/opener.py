46e120e6540b5e263ad4685a91674d6e3ced1e36
vera.k <vera_boris@tut.by>
2022-07-25 20:23:47 +0300

    add yaml parser

--- gendiff/main_logic/opener.py
+++ gendiff/main_logic/opener.py
@@ -1,6 +1,11 @@
 import json
+import yaml
 
 
 def open_(file):
-    data = json.load(open(file))
-    return data
+    if file.endswith('.json'):
+        data = json.load(open(file))
+        return data
+    elif file.endswith('.yml') or file.endswith('.yaml'):
+        data = yaml.safe_load(open(file))
+        return data
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,7 +5,10 @@ import pytest
 
 JSON1 = 'tests/fixtures/file1.json'
 JSON2 = 'tests/fixtures/file2.json'
+YAML1 = 'tests/fixtures/file1.yaml'
+YML2 = 'tests/fixtures/file2.yml'
 RESULT_STYLISH_FLAT = 'tests/fixtures/result_stylish_flat'
+
 
 
 def get_right_result(path_to_file):
@@ -15,7 +18,8 @@ def get_right_result(path_to_file):
 
 
 @pytest.mark.parametrize('file1, file2, format_name, result', [
-    (JSON1, JSON2, STYLISH, RESULT_STYLISH_FLAT)
+    (JSON1, JSON2, STYLISH, RESULT_STYLISH_FLAT),
+    (YAML1, YML2, STYLISH, RESULT_STYLISH_FLAT)
 ])
 def test_gendiff(file1, file2, format_name, result):
     assert generate_diff(file1, file2, format_name) == get_right_result(result)
